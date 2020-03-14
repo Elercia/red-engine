75f1ff61b42e2ddb7f93f8d3a9e2cfd46871c86e
Elercia <rabierv@hotmail.fr>
2020-03-13 20:49:07 -0400

    Remove diligent engine

--- engine/include/RedEngine/engine/RenderingEngine.hpp
+++ engine/include/RedEngine/engine/RenderingEngine.hpp
@@ -4,16 +4,6 @@
 #include <iostream>
 #include <memory>
 
-
-
-#include <DiligentCore/Graphics/GraphicsEngine/interface/DeviceContext.h>
-#include <DiligentCore/Graphics/GraphicsEngine/interface/RenderDevice.h>
-#include <DiligentCore/Graphics/GraphicsEngine/interface/SwapChain.h>
-
-#undef GetObject
-
-#include <DiligentCore/Common/interface/RefCntAutoPtr.h>
-
 namespace red
 {
 class Window;
@@ -28,14 +18,7 @@ public:
 
     void WindowResizeCallBack(uint32_t width, uint32_t height);
 
-    Diligent::DeviceType GetDeviceType() const { return m_DeviceType; }
-
 private:
-    Diligent::RefCntAutoPtr<Diligent::IRenderDevice> m_device;
-    Diligent::RefCntAutoPtr<Diligent::IDeviceContext> m_immediateContext;
-    Diligent::RefCntAutoPtr<Diligent::ISwapChain> m_swapChain;
-    Diligent::RefCntAutoPtr<Diligent::IPipelineState> m_pso;
-    Diligent::DeviceType m_DeviceType = Diligent::DeviceType::OpenGL;
 };
 
 }  // namespace red
