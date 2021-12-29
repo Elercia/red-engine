92bf5f12860d218658eb98e82abb52d6fcf61793
Elercia <rabierv@hotmail.fr>
2021-12-29 16:28:26 +0100

    Fix wrong method usage

--- engine/src/Core/Debug/Logger/Logger.cpp
+++ engine/src/Core/Debug/Logger/Logger.cpp
@@ -3,13 +3,13 @@
 
 namespace red
 {
-void SetLogLevel(LogLevel::Enum level) { GetRedLogger()->SetLogLevel(level); }
+void SetLogLevel(LogLevel level) { GetRedLogger()->SetLogLevel(level); }
 
 Logger::Logger() : m_logLevel(LogLevel::LEVEL_INFO) {}
 
-void Logger::SetLogLevel(LogLevel::Enum level) { m_logLevel = level; }
+void Logger::SetLogLevel(LogLevel level) { m_logLevel = level; }
 
-void Logger::Out(const std::string& data) { std::cout << data << std::endl; }
+// void Logger::Out(const std::string& data) { std::cout << data << std::endl; }
 
 }  // namespace red
 
