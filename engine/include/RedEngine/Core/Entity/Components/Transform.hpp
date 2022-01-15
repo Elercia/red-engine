73cbc60c50c9fe4a550fdf397eafc9673bbf4e54
elercia <rabierv@hotmail.fr>
2022-01-15 20:59:39 +0100

    Improve transform component to store more data and construct a Matrix44 (aka the local world matrix of the entity)

--- engine/include/RedEngine/Core/Entity/Components/Transform.hpp
+++ engine/include/RedEngine/Core/Entity/Components/Transform.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "RedEngine/Math/Matrix.hpp"
 #include "RedEngine/Math/Vector.hpp"
 
 #include "Component.hpp"
@@ -20,17 +21,38 @@ public:
     Transform(Entity* entity);
     Transform(Entity* entity, float x, float y);
     Transform(Entity* entity, Vector2 position);
-    ~Transform() override = default;
+    virtual ~Transform() = default;
 
     [[nodiscard]] const Vector2& GetPosition() const;
     [[nodiscard]] Vector2& GetPosition();
 
+    [[nodiscard]] const Vector2& GetScale() const;
+    [[nodiscard]] Vector2& GetScale();
+
+    [[nodiscard]] float GetRotationDeg() const;
+    [[nodiscard]] float GetRotationRad() const;
+
     void SetPosition(const Vector2& pos);
     void SetPosition(float x, float y);
+    void SetScale(const Vector2& scale);
+    void SetRotationDeg(float rotationDeg);
+    void SetRotationRad(float rotationRad);
+    void SetDepth(float depth);
+
+    void UpdateWorldMatrixIfNeeded();
+    const Matrix44& GetWorldMatrix() const;
+    Matrix44& GetWorldMatrix();
 
 private:
+    // World position
     Vector2 m_position{0.f, 0.f};
-    float m_rotation{0.f};
-    Vector2 m_scale{0.f, 0.f};
+    Vector2 m_scale{1.f, 1.f};
+    float m_rotation{0.f};  // degrees
+    Vector2 m_roationAnchor{0.f, 0.f};
+
+    float m_depth{0.f};
+
+    Matrix44 m_worldMatrix;
+    bool m_dirtyWorldMatrix{true};
 };
 }  // namespace red
--- engine/src/Core/Entity/Components/Transform.cpp
+++ engine/src/Core/Entity/Components/Transform.cpp
@@ -2,6 +2,11 @@
 
 #include "RedEngine/Core/CoreModule.hpp"
 
+#include "RedEngine/Math/Math.hpp"
+#include "RedEngine/Math/Matrix.hpp"
+#include "RedEngine/Math/MatrixFunctions.hpp"
+#include "RedEngine/Math/Vector.hpp"
+
 namespace red
 {
 RED_COMPONENT_BASIC_FUNCTIONS_IMPL(Transform)
@@ -31,11 +36,85 @@ Vector2& Transform::GetPosition()
 void Transform::SetPosition(const Vector2& pos)
 {
     m_position = pos;
+    m_dirtyWorldMatrix = true;
 }
 
 void Transform::SetPosition(float x, float y)
 {
     m_position = Vector2(x, y);
+    m_dirtyWorldMatrix = true;
+}
+
+const Vector2& Transform::GetScale() const
+{
+    return m_scale;
+}
+
+Vector2& Transform::GetScale()
+{
+    return m_scale;
+}
+
+float Transform::GetRotationDeg() const
+{
+    return m_rotation;
+}
+
+float Transform::GetRotationRad() const
+{
+    return Math::ToRadians(m_rotation);
+}
+
+void Transform::SetScale(const Vector2& scale)
+{
+    m_scale = scale;
+    m_dirtyWorldMatrix = true;
+}
+
+void Transform::SetRotationDeg(float rotationDeg)
+{
+    m_rotation = rotationDeg;
+    m_dirtyWorldMatrix = true;
+}
+
+void Transform::SetRotationRad(float rotationRad)
+{
+    m_rotation = Math::ToDegrees(rotationRad);
+    m_dirtyWorldMatrix = true;
+}
+
+void Transform::SetDepth(float depth)
+{
+    m_depth = depth;
+    m_dirtyWorldMatrix = true;
+}
+
+void Transform::UpdateWorldMatrixIfNeeded()
+{
+    if (m_dirtyWorldMatrix)
+    {
+        m_dirtyWorldMatrix = false;
+
+        m_worldMatrix = Matrix44::Identity();
+        m_worldMatrix = Math::Translate(m_worldMatrix, Vector3(m_position, m_depth));
+
+        m_worldMatrix = Math::Translate(m_worldMatrix, Vector3(m_roationAnchor, 0.0f));
+        m_worldMatrix = Math::Rotate(m_worldMatrix, Vector3(0.0f, 0.0f, Math::ToRadians(m_rotation)));
+        m_worldMatrix = Math::Translate(m_worldMatrix, -Vector3(m_roationAnchor, 0.0f));
+        m_worldMatrix = Math::Scale(m_worldMatrix, Vector3(m_scale, 1.0f));
+    }
+}
+
+const Matrix44& Transform::GetWorldMatrix() const
+{
+    return m_worldMatrix;
+}
+
+Matrix44& Transform::GetWorldMatrix()
+{
+    UpdateWorldMatrixIfNeeded();
+
+    return m_worldMatrix;
 }
 
 }  // namespace red
