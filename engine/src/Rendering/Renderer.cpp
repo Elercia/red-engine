1c3623ca2d39971fd75919da9141b0754d4a4f9d
elercia <rabierv@hotmail.fr>
2022-04-09 12:00:41 +0200

    Add opengl debug output to track wrong GL calls

--- engine/src/Rendering/Renderer.cpp
+++ engine/src/Rendering/Renderer.cpp
@@ -32,6 +32,97 @@ const int PrimitiveTypesAsGLTypes[] = {
     GL_TRIANGLES, GL_QUADS, GL_LINES, GL_POINTS, GL_LINE_LOOP, GL_LINE_STRIP, GL_TRIANGLE_STRIP, GL_TRIANGLE_FAN,
 };
 
+static void OpenGLMessageCallback(GLenum source, GLenum type, GLuint /*id*/, GLenum severity, GLsizei /*length*/,
+                                  const GLchar* message, const void* /*userParam*/)
+{
+    const char* sourceStr;
+    const char* typeStr;
+    const char* severityStr;
+
+    switch (source)
+    {
+        case GL_DEBUG_SOURCE_API:
+            sourceStr = "API";
+            break;
+        case GL_DEBUG_SOURCE_WINDOW_SYSTEM:
+            sourceStr = "WINDOW SYSTEM";
+            break;
+        case GL_DEBUG_SOURCE_SHADER_COMPILER:
+            sourceStr = "SHADER COMPILER";
+            break;
+        case GL_DEBUG_SOURCE_THIRD_PARTY:
+            sourceStr = "THIRD PARTY";
+            break;
+        case GL_DEBUG_SOURCE_APPLICATION:
+            sourceStr = "APPLICATION";
+            break;
+        case GL_DEBUG_SOURCE_OTHER:
+            sourceStr = "UNKNOWN";
+            break;
+        default:
+            sourceStr = "UNKNOWN";
+            break;
+    }
+
+    switch (type)
+    {
+        case GL_DEBUG_TYPE_ERROR:
+            typeStr = "ERROR";
+            break;
+        case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR:
+            typeStr = "DEPRECATED BEHAVIOR";
+            break;
+        case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:
+            typeStr = "UDEFINED BEHAVIOR";
+            break;
+        case GL_DEBUG_TYPE_PORTABILITY:
+            typeStr = "PORTABILITY";
+            break;
+        case GL_DEBUG_TYPE_PERFORMANCE:
+            typeStr = "PERFORMANCE";
+            break;
+        case GL_DEBUG_TYPE_OTHER:
+            typeStr = "OTHER";
+            break;
+        case GL_DEBUG_TYPE_MARKER:
+            typeStr = "MARKER";
+            break;
+        default:
+            typeStr = "UNKNOWN";
+            break;
+    }
+
+    bool log = false;
+    switch (severity)
+    {
+        case GL_DEBUG_SEVERITY_HIGH:
+            severityStr = "HIGH SEVERITY";
+            log = true;
+            break;
+        case GL_DEBUG_SEVERITY_MEDIUM:
+            severityStr = "MEDIUM SEVERITY";
+            log = true;
+            break;
+        case GL_DEBUG_SEVERITY_LOW:
+            severityStr = "LOW SEVERITY";
+            log = true;
+            break;
+        case GL_DEBUG_SEVERITY_NOTIFICATION:
+            severityStr = "NOTIFICATION";
+            // log = true;
+            break;
+        default:
+            severityStr = "UNKNOWN";
+            log = true;
+            break;
+    }
+
+    if (log)
+    {
+        RED_LOG_INFO("OpenGL {}::{} raised from {}: {}\n", typeStr, severityStr, sourceStr, message);
+    }
+}
+
 Renderer::Renderer()
     : m_glContext(nullptr)
     , m_window(nullptr)
@@ -77,6 +168,10 @@ void Renderer::InitRenderer(WindowComponent* window)
         RED_LOG_ERROR("Failed to query openGL context from SDL");
         return;
     }
+
+    // During init, enable debug output
+    glEnable(GL_DEBUG_OUTPUT);
+    glDebugMessageCallback(OpenGLMessageCallback, nullptr);
 
     glEnable(GL_DEBUG_OUTPUT);
     glEnable(GL_BLEND);
