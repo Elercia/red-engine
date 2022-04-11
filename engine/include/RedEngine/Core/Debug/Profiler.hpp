8df0ba4e4702fb4945a8b670f1f3819537e4e44d
elercia <rabierv@hotmail.fr>
2022-04-11 23:16:54 +0200

    Rework profiler macro forward to optick

--- engine/include/RedEngine/Core/Debug/Profiler.hpp
+++ engine/include/RedEngine/Core/Debug/Profiler.hpp
@@ -2,34 +2,35 @@
 
 #ifdef RED_USE_PROFILER
 
+#ifdef None
+#undef None  // seems to be define inside X11
+#endif
+
 #include <optick.h>
 
 struct ProfilerCategory
 {
-    static inline Optick::Category::Type Rendering = Optick::Category::Rendering;
-    static inline Optick::Category::Type Input = Optick::Category::Input;
-    static inline Optick::Category::Type Debug = Optick::Category::Input;
-    static inline Optick::Category::Type None = Optick::Category::Input;
+    static inline auto Rendering = Optick::Category::Rendering;
+    static inline auto Input = Optick::Category::Input;
+    static inline auto Debug = Optick::Category::Input;
+    static inline auto None = Optick::Category::Input;
 };
 
-#define PROFILER_GENERATE_APP_PROFILE(...) OPTICK_APP(__VA_ARGS__)
-#define PROFILER_SHUTDOWN(...)             OPTICK_SHUTDOWN(__VA_ARGS__)
-#define PROFILER_FRAME(...)                OPTICK_FRAME(__VA_ARGS__)
-#define PROFILER_EVENT(...)                OPTICK_EVENT(__VA_ARGS__)
+#define PROFILER_GENERATE_APP_PROFILE(...) OPTICK_APP(#__VA_ARGS__)
+#define PROFILER_SHUTDOWN()                OPTICK_SHUTDOWN()
+#define PROFILER_FRAME(...)                OPTICK_FRAME(#__VA_ARGS__)
+#define PROFILER_EVENT(...)                OPTICK_EVENT(#__VA_ARGS__)
 #define PROFILER_EVENT_CATEGORY(MSG, CAT)  OPTICK_CATEGORY(MSG, CAT)
-#define PROFILER_THREAD(...)               OPTICK_THREAD(__VA_ARGS__)
+#define PROFILER_THREAD(...)               OPTICK_THREAD(#__VA_ARGS__)
 
 #else
 
-struct ProfilerCategory
+enum class ProfilerCategory
 {
-    enum Enum
-    {
-        Rendering,
-        Input,
-        Debug,
-        None
-    };
+    Rendering,
+    Input,
+    Debug,
+    None
 };
 
 #define PROFILER_GENERATE_APP_PROFILE(...)
