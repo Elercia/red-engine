5a711d06e85c0be6b69a8bc0b025f750b60a0e96
elercia <rabierv@hotmail.fr>
2022-08-10 19:41:34 +0200

    Fix build script

--- scripts/VS2019Build.bat
+++ scripts/VS2019Build.bat
@@ -3,6 +3,6 @@ echo %1 Build Start
 
 rem Force language to english
 set VSLANG=1033 
-msbuild.exe "scripts/projects/vs2019/RedEngine.sln" /t:Build /p:Configuration=%1 /p:Platform=Win64 /fileLoggerParameters:encoding=UTF-8
+msbuild.exe "scripts/projects/windows_vs2019/RedEngine.sln" /t:Build /p:Configuration=%1 /p:Platform=Win64 /fileLoggerParameters:encoding=UTF-8
 
 echo %1 Build End
