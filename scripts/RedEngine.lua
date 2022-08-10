d06eaf8c233d6773c89aa3d13be5020c1c8d0247
elercia <rabierv@hotmail.fr>
2022-08-10 19:36:17 +0200

    Add compilation flags

--- scripts/RedEngine.lua
+++ scripts/RedEngine.lua
@@ -1,9 +1,9 @@
-outputDirSementic = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}/%{prj.name}"
+outputDirSementic = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}-" .. _ACTION .. "/%{prj.name}"
 
 rootPath 				= "%{wks.location}/../../../"
 enginePath 				= rootPath .. "engine/"
 externalPath 			= rootPath .. "external/"
-projectsFilesLocation 	= "./projects/" .. _ACTION
+projectsFilesLocation 	= "./projects/" .. os.target() .. "_" .. _ACTION
 
 cppDialect 				= "C++20"
 
@@ -27,6 +27,8 @@ if os.istarget("linux") then
 	table.insert(libsToLink, "dl")
 elseif os.istarget("windows") then
 	table.insert(availablePlatforms, "Win64")
+	table.insert(libsToLink, "shell32")
+	table.insert(libsToLink, "Advapi32")
 end
 
 workspace "RedEngine"
@@ -69,8 +71,16 @@ workspace "RedEngine"
 
 	location(projectsFilesLocation)
 
-	filter { "platforms:Win64" }
-		buildoptions { "/Zc:hiddenFriend-" } -- Bug in MSVC : https://github.com/catchorg/Catch2/issues/2174
+	filter { "toolset:msc" }
+  		buildoptions { "/Zc:hiddenFriend-" } -- Bug in MSVC : https://github.com/catchorg/Catch2/issues/2174
+	filter {}
+
+	filter { "toolset:clang" }
+		buildoptions 
+		{ 
+			"-Wno-c++98-compat",
+			"-Wno-c++98-compat-pedantic" 
+		} 
 	filter {}
 
 
