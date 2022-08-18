cb354c95e134cfe7bd1c0bc89100629225cb72f4
elercia <rabierv@hotmail.fr>
2022-08-18 23:22:16 +0200

    Change random to faster method

--- engine/src/Utils/Random.cpp
+++ engine/src/Utils/Random.cpp
@@ -2,25 +2,29 @@
 
 #include "RedEngine/Utils/UtilityModule.hpp"
 
-#include <random>
-
-static std::random_device rd;
-static std::mt19937_64 gen(rd());
-
 namespace red
 {
+static uint64 s_state = 1;
+
 void InitRandomEngine(uint64 seed)
 {
-    gen.seed(seed);
+    s_state = seed;
 }
 
 uint32 RandomUint32()
 {
-    return (uint32) gen();
+    return (uint32)RandomUint64();
 }
 
 uint64 RandomUint64()
 {
-    return gen();
+    uint64 x = s_state;
+	x ^= x << 13;
+	x ^= x >> 7;
+	x ^= x << 17;
+
+    s_state = x;
+
+	return x;
 }
 }  // namespace red
