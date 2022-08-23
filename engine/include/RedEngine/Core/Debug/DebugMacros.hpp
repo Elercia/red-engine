553af9ec958f9cc7720215f96a896b7f6ffce4d7
elercia <rabierv@hotmail.fr>
2022-08-23 19:11:06 +0200

    Make the RedAssert &co macros break at the marco
    Before the assert was made inside the HandleAssert function leading the
    debugger to go to this line instead of the assertion line

--- engine/include/RedEngine/Core/Debug/DebugMacros.hpp
+++ engine/include/RedEngine/Core/Debug/DebugMacros.hpp
@@ -9,17 +9,19 @@ namespace red
 void LogAssert(const char* filename, int line, const std::string& s);
 
 template <typename... Args>
-void HandleAssert(bool expr, const char* filename, int line, const std::string& message = "", Args... args)
+bool HandleAssert(bool expr, const char* filename, int line, const std::string& message = "", Args... args)
 {
     if (expr)
-        return;
+        return false;
 
     std::string logString = fmt::format(message, std::forward<Args>(args)...);
 
     LogAssert(filename, line, logString);
 
 #ifdef RED_BREAK_ON_ASSERT
-    debug_break();
+    return true;
+#else
+    return false;
 #endif
 }
 }  // namespace red
@@ -27,16 +29,18 @@ void HandleAssert(bool expr, const char* filename, int line, const std::string& 
 #ifdef RED_DEBUG
 
 // TODO Add a abort reason (out of memory for exemple)
-#define RedAbort(...)                                               \
-    do                                                               \
-    {                                                                \
-        red::HandleAssert(false, __FILE__, __LINE__, ##__VA_ARGS__); \
+#define RedAbort(...)                                                    \
+    do                                                                   \
+    {                                                                    \
+        if (red::HandleAssert(false, __FILE__, __LINE__, ##__VA_ARGS__)) \
+            debug_break();                                               \
     } while (0);
 
-#define RedAssert(expr, ...)                                       \
-    do                                                              \
-    {                                                               \
-        red::HandleAssert(expr, __FILE__, __LINE__, ##__VA_ARGS__); \
+#define RedAssert(expr, ...)                                            \
+    do                                                                  \
+    {                                                                   \
+        if (red::HandleAssert(expr, __FILE__, __LINE__, ##__VA_ARGS__)) \
+            debug_break();                                              \
     } while (0);
 
 #else
