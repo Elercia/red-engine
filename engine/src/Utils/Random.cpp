a2fe310cbd4a561b3dc8774027e529e6add434d4
elercia <rabierv@hotmail.fr>
2022-09-03 23:34:35 +0200

    Change random engine to xorshift*

--- engine/src/Utils/Random.cpp
+++ engine/src/Utils/Random.cpp
@@ -13,18 +13,32 @@ void InitRandomEngine(uint64 seed)
 
 uint32 RandomUint32()
 {
-    return (uint32)RandomUint64();
+    return (uint32) RandomUint64();
 }
 
+// xorshift64*
 uint64 RandomUint64()
 {
     uint64 x = s_state;
-	x ^= x << 13;
-	x ^= x >> 7;
-	x ^= x << 17;
+    x ^= x >> 12;
+    x ^= x << 25;
+    x ^= x >> 27;
 
-    s_state = x;
+    s_state = x * 0x2545F4914F6CDD1DULL;
 
-	return x;
+    return x;
+}
+
+uint32 RandomRange(uint32 min, uint32 max)
+{
+    const auto fRandValue = (float) RandomUint32() / (float) UINT_MAX;
+
+    return (uint32)(fRandValue * max) + min;
+}
+
+float RandomFloatRange(float min, float max)
+{
+    const auto fRandValue = (float) RandomUint32() / (float) UINT_MAX;
+    return ( fRandValue * max) + min;
 }
 }  // namespace red
